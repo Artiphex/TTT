a5a1f34dbd15702491595d8a44abea93b2964840
CovexAlex <covex.alex@gmail.com>
2014-05-31 08:43:36 +0200

    License

--- src/Commands/StartCommand.java
+++ src/Commands/StartCommand.java
@@ -11,19 +11,18 @@ import de.iron42.TTT.TTT;
 public class StartCommand implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(CommandSender cs, Command cmd, String label,
-			String[] args) {
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 		if (label.equalsIgnoreCase("start")) {
 			if (TTT.Status == GameStatus.Lobby) {
 				if (cs.isOp()) {
-					
+
 					TTT.Timer = 11;
-					
+
 					Bukkit.broadcastMessage("§8[§4TTT§8] §7The game was forced started!");
 				}
 			}
 		}
 		return false;
 	}
-	
+
 }
